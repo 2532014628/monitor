d6102e6f1e5edd91838cbbd73906b85ebabfd6fa
yhQin-gd <1701339239@qq.com>
2025-03-20 10:45:15 +0800

    把system_info迁移到TDengine和修改了相应的函数(尚未完成)

--- backend/server/main.go
+++ backend/server/main.go
@@ -38,6 +38,12 @@ func main() {
 	os.Setenv("DB_HOST", config.DB.Host)
 	os.Setenv("DB_PORT", config.DB.Port)
 	os.Setenv("DB_NAME", config.DB.Name)
+	//设置TDengine数据库连接的环境变量
+	os.Setenv("TDENGINE_USER", config.TDengine.User)
+	os.Setenv("TDENGINE_PASSWORD", config.TDengine.Password)
+	os.Setenv("TDENGINE_HOST", config.TDengine.Host)
+	os.Setenv("TDENGINE_PORT", config.TDengine.Port)
+	os.Setenv("TDENGINE_NAME", config.TDengine.Name)
 	// OSS服务
 	os.Setenv("OSS_REGION", config.OSS.OSS_REGION)
 	os.Setenv("OSS_ACCESS_KEY_ID", config.OSS.OSS_ACCESS_KEY_ID)
@@ -66,10 +72,16 @@ func main() {
 	if err := db.ConnectDatabase(); err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
+	//连接TDengine数据库
+	if err := db.ConnectTDengine(); err != nil {
+		log.Fatalf("Failed to connect to TDengine database: %v", err)
+	}
+
 	// 初始化数据库
 	if err := db.InitDB(); err != nil {
 		log.Fatalf("Failed to initialize database: %v", err)
 	}
+	
 	// 初始化数据库数据
 	if err := db.InitDBData(); err != nil {
 		log.Fatalf("Failed to initialize data: %v", err)
